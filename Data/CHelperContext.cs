f6bad81411d28ea1bb91f9c6a0bedc4611fe9c50
MattisTrygstad <mattislt@sf-nett.no>
2020-06-17 10:01:48 +0200

    added DB connection

---
+++ Data/CHelperContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CHelper.Data
+{
+    public class CHelperContext : DbContext
+    {
+        public CHelperContext(DbContextOptions<CHelperContext> opt) : base()
+        {
+
+        }
+    }
+}
